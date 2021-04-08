bc33606ce60603a693ae2fddc856ac7b23d50c82
Thomas Stensitzki <thomas@mcsmemail.de>
2021-04-08 13:31:06 +0200

    Update

--- Update-CASMailbox.ps1
+++ Update-CASMailbox.ps1
@@ -85,7 +85,16 @@ Param(
 
 Set-StrictMode -Version Latest
 
-Import-Module GlobalFunctions
+# Import GlobalFunctions
+if($null -ne (Get-Module -Name GlobalFunctions -ListAvailable).Version) {
+  Import-Module -Name GlobalFunctions
+}
+else {
+  Write-Warning -Message 'Unable to load GlobalFunctions PowerShell module.'
+  Write-Warning -Message 'Open an administrative PowerShell session and run Import-Module GlobalFunctions'
+  Write-Warning -Message 'Please check http://bit.ly/GlobalFunctions for further instructions'
+  exit
+}
 $ScriptDir = Split-Path $script:MyInvocation.MyCommand.Path
 $ScriptName = $MyInvocation.MyCommand.Name
 $logger = New-Logger -ScriptRoot $ScriptDir -ScriptName $ScriptName -LogFileRetention 14
